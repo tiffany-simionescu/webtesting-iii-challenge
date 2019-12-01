5ae195fe490c8bbd8bdb977ee4899f943cce3b90
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-11-30 22:28:58 -0500

    Added tests for Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+
+import React from 'react';
+import * as rtl from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+test('Dashboard renders without crashing', () => {
+  rtl.render(<Dashboard />);
+});
+
+test('<Dashboard /> snapshot', () => {
+  const wrapper = rtl.render(<Dashboard />);
+  expect(wrapper.asFragment()).toMatchSnapshot();
+})
