66274f0ba10067c89a50d6814d160de5aafb9667
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-12-02 17:10:36 -0500

    Finished Display.spec.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,14 +2,25 @@
 
 import React from 'react';
 import * as rtl from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
 import Dashboard from './Dashboard';
 
+// Dashboard MVP
 test('Dashboard renders without crashing', () => {
   rtl.render(<Dashboard />);
 });
 
+// Dashboard MVP
 test('<Dashboard /> snapshot', () => {
   const wrapper = rtl.render(<Dashboard />);
   expect(wrapper.asFragment()).toMatchSnapshot();
-})
+});
+
+// Gate MVP
+test('Gate defaults to unlocked and open', () => {
+
+});
+
+// Gate MVP
+test('Gate cannot be closed or opened if it is locked', () => {
+
+});
